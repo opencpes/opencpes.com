c8efad921a6fe8eff376ab83e0e1e2aba519d141
Kurt Seifried <kseifried@cloudsecurityalliance.org>
2019-06-26 11:21:53 -0600

    more docs updates

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# The configuration files for this are stored in ~/.csalabs-opencpes.com/config
+#
+# 1) You will need to use either your default SSH key or specify one in the
+# configuration file
+# SSHKEY=
+#
+# 2) You will need to use either your default AWS credentials or specify one in
+# the configuration file
+# AWS_ACCESS_KEY_ID="<id>"
+# AWS_SECRET_ACCESS_KEY="<key>"
+# no need for AWS_DEFAULT_REGION as S3 is global
+
+# 3) You will need to specify the S3 bucket if doing updates
+# S3_BUCKET_NAME="s3_bucket_name"
+#
+# 4) You will need to use either you default Cloudflare credentials or specify
+# one in the configuration file
+# 5) You will need to specify the domain name to update in the config file
+# There is no good Cloudflare CLI docker image so we'll have to create one from scratch =(.
+
 # STAGE 1
 # Git pull and remove files that get blown away by the build
 # we assume you have git installed and a key to pull/push from 
