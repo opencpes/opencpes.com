27b2b8cd88d1ac30776aa73796c60c7af74ee7df
Kurt Seifried <kseifried@cloudsecurityalliance.org>
2019-06-26 11:05:25 -0600

    further updates to the deployment process

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# ensure we have latest opencpes.com
+
+# STAGE 1
+# Git pull and remove files that get blown away by the build
+# we assume you have git installed and a key to pull/push from 
+# https://github.com/opencpes/opencpes.com otherwise how did you even get here?
+#
 git pull
 
 # remove the precache manifest since it gets blown away anyways
@@ -7,17 +12,26 @@ git rm -rf build/precache-manifest.[0-9a-f]*.js
 git commit -m "removed precache-manifest" 
 git push
 
+# STAGE 2
+# Startup a Docker container with nodejs and manually cal it to use the build-script.sh
+#
+# Optional: first time you'll need to build the container:
+# docker build . -t opencpes-build -f CURRENT_DIR/build-and-deploy/docker-node-build/Dockerfile
+
 CURRENT_DIR=`pwd`
-#docker build . -t opencpes-build -f CURRENT_DIR/Dockerfile
 docker run -v $CURRENT_DIR:/opencpes.com -it  opencpes-build  /build-script.sh
 
-# update git build dir #
+# now that the build is done let's push the new files into git
 git add -A build/*
 git commit -m "npm run build 2019-06-25-13-00" build/*
 git push
 
-### create a new S3 bucket
-# s3 synch /build to the S3 bucket
-# set permissions to public on the s3 bucket
-### update Cloudflare DNS for opencpes.com to the new bucket DNS (CNAME)
-# open in browser and test - Website Watcher?
+# STAGE 3 
+# short term: we just update the S3 bucket and we're done
+# long term: we create a new S3 bucket and update the Cloudflare DNS to point at it, 
+# and create a new Cloudflare entry for the previous bucket, this allows us to easily roll
+# back if the website gets broken (short term: we roll git back and rebuild)
+
+# STAGE 4
+# we run the website through WebsiteWatcher (WSW) running in AWS workspace, this is
+# a manual task
